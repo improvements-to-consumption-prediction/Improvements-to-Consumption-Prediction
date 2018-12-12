548be0d31f0c1fb7081896ea93d3f9342c514ea7
improvements-to-consumption-prediction <45808818+improvements-to-consumption-prediction@users.noreply.github.com>
2018-12-11 22:10:21 -0600

    Update ml_models_final_v2.py

--- scripts/ml_models_final_v2.py
+++ scripts/ml_models_final_v2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import ShuffleSplit
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import mannwhitneyu
 
@@ -82,3 +82,52 @@ for i in range(5):
 print(np.mean(reduced_mse))
 print(np.mean(full_mse))
 print(mannwhitneyu(reduced_mse, full_mse))
+
+
+# final estimates of "reduced" random forest model
+rf_reduced_final = RandomForestRegressor()
+
+cv = ShuffleSplit(n_splits=5, test_size=0.2)
+
+final_reduced_mse = []
+final_reduced_r2 = []
+final_reduced_adj_r2 = []
+
+for train_idx, test_idx in cv.split(Xs_reduced):
+    Xs_train, Xs_test = Xs_reduced[train_idx], Xs_reduced[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+    rf_reduced_final.fit(Xs_train, y_train)
+    y_pred = rf_reduced_final.predict(Xs_reduced)
+    mse = mean_squared_error(y_pred, y)
+    r2 = r2_score(y_pred, y)
+    adj_r2 = ((1-r2) * (Xs_train.shape[0]-1))/ (Xs_train.shape[0] - Xs_train.shape[1] - 1)
+    final_reduced_r2.append(r2)
+    final_reduced_adj_r2.append(adj_r2)
+    final_reduced_mse.append(mse)
+
+print(f"Reduced RF Model R^2: {round(np.mean(final_reduced_r2), 4)}")
+print(f"Reduced RF Model Adj. R^2: {round(np.mean(final_reduced_adj_r2), 4)}")
+print(f"Reduced RF Model MSE: {round(np.mean(final_reduced_mse), 8)}")
+
+# final estimates of "full" RF model
+rf_full_final = RandomForestRegressor(n_estimators=50000)
+
+final_full_mse = []
+final_full_r2 = []
+final_full_adj_r2 = []
+
+for train_idx, test_idx in cv.split(Xs_full):
+    Xs_train, Xs_test = Xs_full[train_idx], Xs_full[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+    rf_full_final.fit(Xs_train, y_train)
+    y_pred = rf_full_final.predict(Xs_full)
+    mse = mean_squared_error(y_pred, y)
+    r2 = r2_score(y_pred, y)
+    adj_r2 = ((1-r2) * (Xs_train.shape[0]-1))/ (Xs_train.shape[0] - Xs_train.shape[1] - 1)
+    final_full_r2.append(r2)
+    final_full_adj_r2.append(adj_r2)
+    final_full_mse.append(mse)
+
+print(f"Full RF Model R^2: {round(np.mean(final_full_r2), 4)}")
+print(f"Full RF Model Adj. R^2: {round(np.mean(final_full_adj_r2), 4)}")
+print(f"Full RF Model MSE: {round(np.mean(final_full_mse), 8)}")
